65c54b55d30ef737898459e109cba3433f5cc8ef
Gus King <gusssk@gmail.com>
2014-03-24 22:20:26 -0600

    Added Gemfile. Made an initial test.

---
+++ Gemfile
@@ -0,0 +1,5 @@
+source "https://rubygems.org"
+gem 'rspec', '~> 2.14.1'
+gem 'launchy', '~> 2.4.2'
+gem 'sinatra', '~> 1.4.4'
+gem 'capybara', '~> 2.2.1'
---
+++ spec/item_spec.rb
@@ -0,0 +1,13 @@
+require 'spec_helper'
+require 'capybara/rspec'
+require_relative '../app'
+
+Capybara.app = App
+
+feature 'user can manage items' do
+  scenario 'user can add an item' do
+    visit '/'
+    expect(page).to have_content("Channa Masala")
+
+  end
+end
